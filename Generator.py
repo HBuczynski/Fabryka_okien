4e89449332a71a3dbbf50c178a6cbd748761c5b4
Łukasz K <luktor99@gmail.com>
2017-05-16 00:19:15 +0200

    Added params to Generator

--- Generator.py
+++ Generator.py
@@ -3,35 +3,66 @@ import random
 import Database
 
 NAMES = ["Beata", "Anna", "Magda", "Zosia", "Karolina", "Grazyna", "Alicja", "Kasia", "Monika"]
+PARAMS = [["Kolor segmentu", ["Czerwony", "Zielony", "Źólty", "Brązowy", "Biały", "Szary", "Niebieski"]],
+          ["Typ drewna", ["Buk", "Sosna", "Dąb", "Jesion", "Świerk", "Modrzew"]],
+          #...
+          ]
 
 # Liczba generowanych danych
 N_MODELS = 3
 N_MAXSEGMENTS = 5
+N_MAXPARAMS = 2
 
 class Generator:
     def __init__(self, database: Database.Database):
         self.db = database
 
     def generate(self):
+        # Generowanie oferty (modele, segmenty, parametry, wartosci)
+        self.generate_stock()
+        # Generowanie klientow
+        self.generate_clients()
+        # Generowanie zamowien
+        self.generate_orders()
+
+    def generate_stock(self):
         print("Generating random data...")
 
         # Wybierz N_MODELS unikalnych nazw z bazy nazw modeli
-        model_names = [NAMES[i] for i in random.sample(range(len(NAMES)), N_MODELS)]
+        model_names = self.generate_names()
 
         for model_name in model_names:
             print("ADD_MODEL: " + model_name)
             self.db.add_model(model_name)
             model_id = self.db.get_model_id(model_name)
 
-            segment_names = self.generateSegments()
+            segment_names = self.generate_segments()
             for segment_name in segment_names:
                 print("\tADD_SEGMENT: " + segment_name)
                 self.db.add_segment(model_name, segment_name, self.generateCenaB(), self.generateCenaC(),
                                     self.generateCenaD())
                 segment_id = self.db.get_segment_id(model_name, segment_name)
 
-    def generateSegments(self):
+                parameters = self.generate_parameters()
+                for parameter in parameters:
+                    print("\t\tADD PARAM: " + parameter[0])
+
+    def generate_clients(self):
+        pass
+        # TODO
+
+    def generate_orders(self):
+        pass
+        # TODO
+
+    def generate_names(self):
+        return [NAMES[i] for i in random.sample(range(len(NAMES)), N_MODELS)]
+
+    def generate_segments(self):
         return ["Segment " + chr(i + 64) for i in range(1, random.randint(2, N_MAXSEGMENTS))]
+
+    def generate_parameters(self):
+        return [PARAMS[i] for i in random.sample(range(len(PARAMS)), random.randint(0, N_MAXPARAMS))]
 
     def generateCenaA(self):
         return 0
