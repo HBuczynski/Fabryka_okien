47e48065c3d2c09d48bbad2c57063efb2c00c83c
Hubert <hubert.buczynski94@gmail.com>
2017-06-10 14:51:32 +0200

    Added actions to buttons.

--- app/gui.py
+++ app/gui.py
@@ -6,10 +6,10 @@ from PyQt5.QtWidgets import QMainWindow
 
 TABLE_WIDGET_COLUMNS_WIDTH = [0.1, 0.3, 0.1, 0.1, 0.2, 0.2]
 
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
 from ui.mainwindow import Ui_MainWindow
+from PyQt5.QtCore import *
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -17,16 +17,44 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Set up the user interface from Designer.
         self.setupUi(self)
         # Setting up additional options (apart from generated, ex: PushButton  action):
-        self.pushButton_DodajFakture.clicked.connect(self.pushButton_DodajFakture)
+
+        # Actions for orders:
+        self.addInvoiceButton.clicked.connect(self.clickedInvoiceButton)
+        self.editInvoiceButton.clicked.connect(self.clickedEditInvoiceButton)
+        self.searchInvoiceButton.clicked.connect(self.clickedInvoiceButton)
+        self.selectInvoiceButton.clicked.connect(self.clickedSelectInvoiceButton)
+
+        # Actions for clients
+        self.addClientButton.clicked.connect(self.clickedAddClientButton)
+        self.editClientButton.clicked.connect(self.clickedEditClientButton)
+        self.searchClientButton.clicked.connect(self.clickedSearchClientButton)
 
     #Definitions of PushButtons action functions:
-    def pushButton_DodajFakture(self):
+    def clickedInvoiceButton(self):
         print("Ddddd")
 
         # TODO: To nie działa
         # self.tableWidget.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         # self.resizeTableWidget()
         # self.pushButton.clicked.connect(self.resizeTableWidget)
+
+    def clickedEditInvoiceButton(self):
+        print("Edcyja")
+
+    def clickedSearchInvoiceButton(self):
+        print("Lupa")
+
+    def clickedSelectInvoiceButton(self):
+        print("Pokaz Zamowienia")
+
+    def clickedAddClientButton(self):
+        print("add client")
+
+    def clickedEditClientButton(self):
+        print("edit client")
+
+    def clickedSearchClientButton(self):
+        print("Search button")
 
     def resizeTableWidget(self):
         tablewidth = self.verticalLayout_1.contentsRect().width()
